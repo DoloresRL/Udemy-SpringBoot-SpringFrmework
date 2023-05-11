2a0deef2cc4b663a98c029ce9c647eba2ce78ab7
DoloresRL <131316458+DoloresRL@users.noreply.github.com>
2023-05-11 16:18:07 -0600

    Ejercicio para directiva if de Thymeleaf (perfil y listar)

--- src/main/java/com/bolsadeideas/springboot/web/app/contollers/IndexContoller.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/contollers/IndexContoller.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.web.app.contollers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,9 +28,19 @@ public class IndexContoller {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Juan");
 		usuario.setApellido("Perez");
+		usuario.setEmail("JuanPerez@correo.com");
 		model.addAttribute("usuario",usuario);
 		model.addAttribute("titulo", "Perfil de usuario ".concat(usuario.getNombre()));
 		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		model.addAttribute("titulo", "Lista de usuarios ");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
+		
 	}
 
 }
